dede7745c1cd482ee7919aecdcd7a5881c09d85a
Kadi Kraman <kadi.kraman@formidable.com>
2019-03-03 20:47:44 +0000

    Add serverless to dev dependencies

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+echo "Deploying!"
+echo this is a variable - ${TEST}
+
+# uncomment v to run the deploy script
+# ./node_modules/.bin/sls config credentials --provider aws --key ${AWS_KEY} --secret ${AWS_SECRET}
+# ./node_modules/.bin/sls deploy -v
